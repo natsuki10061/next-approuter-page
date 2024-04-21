c9558cf0f1ce0f639c66b8ea849aad4bf4b82c7a
hayato94087 <hayato94087@gmail.com>
2024-04-21 13:03:58 +0900

    feat:Dynamic RouteのDynamic Segmentの値を取得

---
+++ src/app/product/[category]/[item]/page.tsx
@@ -0,0 +1,74 @@
+import { FC } from "react";
+
+type Props = {
+  params: {
+    category: string;
+    item: string;
+  };
+};
+
+const Page: FC<Props> = (props) => {
+  return (
+    <>
+      <div className="p-5">
+        <div className="p-5 bg-blue-100">
+          <header className="p-5 bg-red-100 flex flex-col space-y-2">
+            <h1 className="text-lg font-bold">タイトル</h1>
+          </header>
+          <main className="p-5 bg-green-100 flex flex-col space-y-5">
+            <section className="flex flex-col space-y-3 bg-yellow-100 p-5">
+              <div className="flex flex-row">
+                <h2 className="text-sm text-blue-800 py-2 px-3 bg-blue-100 rounded-md">
+                  props
+                </h2>
+              </div>
+              <div className="flex flex-row">
+                <pre className="w-full text-sm text-white py-2 px-3 bg-slate-800 rounded-md">
+                  {JSON.stringify(props)}
+                </pre>
+              </div>
+            </section>
+            <section className="flex flex-col space-y-3 bg-yellow-100 p-5">
+              <div className="flex flex-row">
+                <h2 className="text-sm text-blue-800 py-2 px-3 bg-blue-100 rounded-md">
+                  props.params
+                </h2>
+              </div>
+              <div className="flex flex-row">
+                <pre className="w-full text-sm text-white py-2 px-3 bg-slate-800 rounded-md">
+                  {JSON.stringify(props.params)}
+                </pre>
+              </div>
+            </section>
+            <section className="flex flex-col space-y-3 bg-yellow-100 p-5">
+              <div className="flex flex-row">
+                <h2 className="text-sm text-blue-800 py-2 px-3 bg-blue-100 rounded-md">
+                  props.params.category
+                </h2>
+              </div>
+              <div className="flex flex-row">
+                <pre className="w-full text-sm text-white py-2 px-3 bg-slate-800 rounded-md">
+                  {JSON.stringify(props.params.category)}
+                </pre>
+              </div>
+            </section>
+            <section className="flex flex-col space-y-3 bg-yellow-100 p-5">
+              <div className="flex flex-row">
+                <h2 className="text-sm text-blue-800 py-2 px-3 bg-blue-100 rounded-md">
+                  props.params.item
+                </h2>
+              </div>
+              <div className="flex flex-row">
+                <pre className="w-full text-sm text-white py-2 px-3 bg-slate-800 rounded-md">
+                  {JSON.stringify(props.params.item)}
+                </pre>
+              </div>
+            </section>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Page;
